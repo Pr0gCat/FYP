b43136a77d9d9d36b085df70b20bbb12a57ef87b
ProgCat <progcat@protonmail.com>
2021-08-18 15:41:12 +0800

    updated firmware

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -5,14 +5,16 @@
 #define STEP_PIN 12
 
 #define UPPER_ENDSTOP_PIN 10
+#define LOWER_ENDSTOP_PIN 9
 
 AccelStepper m1(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
 
 void setup() {
   Serial.begin(9600);
-  while(!Serial.available());
+  // while(!Serial.available());
   // put your setup code here, to run once:
   pinMode(UPPER_ENDSTOP_PIN, INPUT_PULLUP);
+  pinMode(LOWER_ENDSTOP_PIN, INPUT_PULLUP);
 
   pinMode(EN_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
@@ -24,51 +26,48 @@ void setup() {
   m1.setMaxSpeed(2400);
   m1.setAcceleration(50);
 
-  // 要確保開機時不是到頂！
-
-  // to the very bottom
+  // 移到底
   m1.setSpeed(-1000);
-  while(!digitalRead(UPPER_ENDSTOP_PIN)){
+  while(digitalRead(LOWER_ENDSTOP_PIN)){
     m1.runSpeed();
-    if(digitalRead(UPPER_ENDSTOP_PIN)){delay(50);}
+    if(!digitalRead(LOWER_ENDSTOP_PIN)){delay(50);}
   }
-  delay(50);
-  // m1.stop();
-  // homing
+
   m1.setSpeed(400);
-  while(digitalRead(UPPER_ENDSTOP_PIN)){
+  while(!digitalRead(LOWER_ENDSTOP_PIN)){
     m1.move(400);
     m1.run();
-    if(!digitalRead(UPPER_ENDSTOP_PIN)){delay(50);}
+    if(digitalRead(LOWER_ENDSTOP_PIN)){delay(50);}
   }
 
   m1.setCurrentPosition(0);
   Serial.println(m1.currentPosition());
   delay(500);
+  // probe height
   m1.setSpeed(1000);
-  while(!digitalRead(UPPER_ENDSTOP_PIN)){
+  while(digitalRead(UPPER_ENDSTOP_PIN)){
     m1.runSpeed();
-    // if(digitalRead(UPPER_ENDSTOP_PIN)){delay(20);}
+    if(!digitalRead(UPPER_ENDSTOP_PIN)){delay(50);}
   }
   delay(500);
+
   m1.setSpeed(400);
-  while(digitalRead(UPPER_ENDSTOP_PIN)){
+  while(!digitalRead(UPPER_ENDSTOP_PIN)){
     m1.move(-400);
     m1.run();
-    if(!digitalRead(UPPER_ENDSTOP_PIN)){delay(50);}
+    if(digitalRead(UPPER_ENDSTOP_PIN)){delay(50);}
   }
   Serial.println(m1.currentPosition());
 
   m1.setAcceleration(100);
   m1.setSpeed(-1000);
-  // m1.setMaxSpeed(2400);
+  m1.setMaxSpeed(2400);
   m1.moveTo(0);
-  // m1.disableOutputs();
+  m1.disableOutputs();
 }
 
-
 void loop() {
-  // Serial.println(digitalRead(UPPER_ENDSTOP_PIN));
+  // Serial.println(digitalRead(LOWER_ENDSTOP_PIN));
   // if(digitalRead(UPPER_ENDSTOP_PIN)){
   //   m1.moveTo(0);
   // }
