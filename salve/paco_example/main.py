7b59ffc804867f67bb0216b48a561f09dca468ff
BuildTools <unconfigured@null.spigotmc.org>
2021-08-19 23:47:59 +0800

    PYserial receive add delay(0.2)

--- salve/paco_example/main.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-if __name__ == '__main__':
-    port = serial.Serial('/dev/ttyUSB0', baudrate=115200, xonxoff=False, rtscts=False)
-    # write a byte
-    port.write(0x80)
-    # read a byte
-    port.read()
