60a4ff4d27f16621ad908c4581a576c19f85cac0
TimidCactus <45101408+TimidCactus@users.noreply.github.com>
2021-08-30 17:09:10 +0800

    Add files via upload

---
+++ firmware/wheel_controller.ino
@@ -0,0 +1,181 @@
+#include<PIDController.h>
+
+#define UPDATA_TIME 100
+
+#define __Kp_count_l 260 
+#define __Ki_count_l 2.7 
+#define __Kd_count_l 2000 
+
+#define __Kp_count_r 260 
+#define __Ki_count_r 2.7 
+#define __Kd_count_r 2000 
+
+//done
+#define __Kp_l 1.88
+#define __Ki_l 0.05
+#define __Kd_l 2
+
+#define __Kp_r 1.81
+#define __Ki_r 0.03
+#define __Kd_r 3
+
+volatile long int speed_count_l = 0, speed_count_r = 0;
+volatile long int distance_l = 0, distance_r = 0;
+int motor_enable_r = 0 ,motor_enable_l = 0;
+PIDController pid_speed_l, pid_speed_r;
+PIDController pid_distance_l, pid_distance_r;
+
+void encoder_l() {
+  if (digitalRead(ENCODER_LR_PIN) == HIGH) {speed_count_l++;distance_l++;}
+  else {speed_count_l--;distance_l--;}
+}
+
+void encoder_r() {
+  if (digitalRead(ENCODER_RR_PIN) == HIGH) {speed_count_r++;distance_r++;}
+  else {speed_count_r--;distance_r--;}
+}
+
+void Set_motor_pwm(int motor_pwm, int motot, int sw, int power)
+{
+  if(sw) {
+    if(power > 0){
+      analogWrite(motor_pwm, power);
+      digitalWrite(motot, LOW);
+    } else {
+      analogWrite(motot, abs(power));
+      digitalWrite(motor_pwm, LOW);
+    }
+  } else {
+    analogWrite(motor_pwm, LOW);
+    digitalWrite(motot, LOW);
+  }
+}
+
+void stop_motor_l()
+{
+  //sw = 0;
+}
+
+void stop_motor_r()
+{
+  //sw = 0;
+}
+
+void set_origin_l()
+{
+  distance_l = 0;
+}
+
+void set_origin_r()
+{
+  distance_r = 0;
+}
+
+void set_distance_l(int distance_l)
+{
+  pid_distance_l.setpoint( motor_distance_l );
+}
+
+void set_distance_r(int distance_r)
+{
+  pid_distance_r.setpoint( motor_distance_r );
+}
+
+void set_speed_l(int speed_l)
+{
+  pid_speed_l.setpoint( speed_l );
+}
+
+void set_speed_r(int speed_r)
+{
+  pid_speed_r.setpoint( speed_r );
+}
+
+int motor_controller_l(int enable)
+{
+  int motor_l_pwm_PIN = pid_speed_l.compute(speed_count_l);
+  int motor_on_l = pid_distance_l.compute(distance_l);
+  speed_count_l = 0;
+  
+  if( motor_on_l > 0){
+    Set_motor_pwm(MOTOR_L_IN1_PIN, MOTOR_L_IN2_PIN, enable, motor_l_pwm );
+    return 1;
+  } 
+  if( motor_on_l < 0) {
+    Set_motor_pwm(MOTOR_L_IN1_PIN, MOTOR_L_IN2_PIN, enable, motor_l_pwm * -1);
+    return 1;
+  }
+  if( motor_on_l == 0) {
+    Set_motor_pwm(MOTOR_L_IN1_PIN, MOTOR_L_IN2_PIN, 0, 0);
+    return 0;
+  }
+  
+}
+
+int motor_controller_r(int enable)
+{
+  int motor_r_pwm = pid_speed_r.compute(speed_count_r);
+  int motor_on_r = pid_distance_r.compute(distance_r);
+  speed_count_r = 0;
+
+  if( motot_on_r > 0){
+    Set_motor_pwm(MOTOR_R_IN1_PIN, MOTOR_R_IN2_PI, enable, motor_r_pwm);
+    return 1;
+  } 
+  if( motot_on_r < 0){
+    Set_motor_pwm(MOTOR_R_IN1_PIN, MOTOR_R_IN2_PI, enable, motor_r_pwm * -1);
+    return 1;
+  } 
+  if( motor_on_r == 0) {
+    Set_motor_pwm(MOTOR_R_IN1_PIN, MOTOR_R_IN2_PI, 0, 0);
+    return 0;
+  }
+  
+}
+
+void wheel_updata()
+{
+  static last_time = millis();
+  if(millis() - last_time  < UPDATA_TIMElast_time) return;
+  motor_controller_l(motor_enable_l);
+  motor_controller_r(motor_enable_r);
+  last_time = millis();  
+}
+
+void setup() {Serial.begin(115200); 
+  // encoder
+  pinMode(ENCODER_LF_PIN, INPUT); 
+  pinMode(ENCODER_LR_PIN, INPUT); 
+  pinMode(ENCODER_RF_PIN, INPUT); 
+  pinMode(ENCODER_RR_PIN, INPUT); 
+  // motor
+  pinMode(MOTOR_L_IN1_PIN, OUTPUT); 
+  pinMode(MOTOR_R_IN1_PIN, OUTPUT); 
+  pinMode(MOTOR_L_IN2_PIN, OUTPUT); 
+  pinMode(MOTOR_R_IN2_PI, OUTPUT); 
+  
+  attachInterrupt(digitalPinToInterrupt(ENCODER_LF_PIN), encoder_l, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_RF_PIN), encoder_r, RISING);
+  
+  pid_speed_l.begin(); 
+  pid_speed_l.tune(__Kp_l , __Ki_l , __Kd_l); 
+  pid_speed_l.limit(-255, 255); 
+  
+  pid_speed_r.begin(); 
+  pid_speed_r.tune(__Kp_r , __Ki_r , __Kd_r); 
+  pid_speed_r.limit(-255, 255); 
+  
+  pid_distance_l.begin(); 
+  pid_distance_l.tune(__Kp_count_l , __Ki_count_l , __Kd_count_l); 
+  pid_distance_l.limit(-255, 255); 
+  
+  pid_distance_r.begin(); 
+  pid_distance_r.tune(__Kp_count_r , __Ki_count_r , __Kd_count_r); 
+  pid_distance_r.limit(-255, 255); 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  wheel_updata();
+}
