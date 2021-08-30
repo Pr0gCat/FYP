173b2bc247d061b4f81fb5c2fcd4b608ca94e88c
xKAIRAx <41248128+xKAIRAx@users.noreply.github.com>
2021-08-30 18:21:45 +0800

    Add files via upload

--- jetson_nano/Mix.py
+++ jetson_nano/Mix.py
@@ -3,9 +3,32 @@ import numpy as np
 from pyzbar.pyzbar import decode
 
 
+def slope_compare(line1, line2):
+    x1 = line1[1]
+    y1 = line1[2]
+    x2 = line1[3]
+    y2 = line1[4]
+
+    x3 = line2[1]
+    y3 = line2[2]
+    x4 = line2[3]
+    y4 = line2[4]
+
+    k1 = 0.0
+    k2 = 0.0
+    if x2 - x1:
+        k1 = (y2 - y1) * 1.0 / (x2 - x1)
+    if x4 - x3:
+        k2 = (y4 - y3) * 1.0 / (x4 - x3)
+    if abs(k2 - k1) > 0.3:
+        return True
+    else:
+        return False
+
+
 #  Return list of None
-def cross_point(line1, line2):  # 计算交点函数
-    x1 = line1[1]  # 取四点坐标
+def cross_point(line1, line2):
+    x1 = line1[1]
     y1 = line1[2]
     x2 = line1[3]
     y2 = line1[4]
@@ -16,12 +39,12 @@ def cross_point(line1, line2):  # 计算交点函数
     y4 = line2[4]
     if (x4 - x3) == 0 and (x2 - x1) == 0:
         return False
-    elif (x4 - x3) == 0:  # L2直线斜率不存在操作
+    elif (x4 - x3) == 0:
         k2 = None
         b2 = 0
         x = x3
-        k1 = (y2 - y1) * 1.0 / (x2 - x1)  # 计算k1,由于点均为整数，需要进行浮点数转化
-        b1 = y1 * 1.0 - x1 * k1 * 1.0  # 整型转浮点型是关键
+        k1 = (y2 - y1) * 1.0 / (x2 - x1)
+        b1 = y1 * 1.0 - x1 * k1 * 1.0
         y = k1 * x * 1.0 + b1 * 1.0
     elif (x2 - x1) == 0:
         k1 = None
@@ -31,9 +54,9 @@ def cross_point(line1, line2):  # 计算交点函数
         b2 = y3 * 1.0 - x3 * k2 * 1.0
         y = k2 * x * 1.0 + b2 * 1.0
     else:
-        k1 = (y2 - y1) * 1.0 / (x2 - x1)  # 计算k1,由于点均为整数，需要进行浮点数转化
-        k2 = (y4 - y3) * 1.0 / (x4 - x3)  # 斜率存在操作
-        b1 = y1 * 1.0 - x1 * k1 * 1.0  # 整型转浮点型是关键
+        k1 = (y2 - y1) * 1.0 / (x2 - x1)
+        k2 = (y4 - y3) * 1.0 / (x4 - x3)
+        b1 = y1 * 1.0 - x1 * k1 * 1.0
         b2 = y3 * 1.0 - x3 * k2 * 1.0
         if k1 - k2 == 0:
             return False
@@ -46,10 +69,9 @@ def cross_point(line1, line2):  # 计算交点函数
 def cross_detect(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray, 60, 150, apertureSize=3)
-
     lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
     if lines is None:
-        return
+        return None
     line1 = [False, 0, 0, 0, 0]
     line2 = [False, 0, 0, 0, 0]
     for line in lines:
@@ -79,13 +101,20 @@ def cross_detect(image):
                         line2[4] = y2
                         cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 10)
             else:
-                cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 5)
-    cross = cross_point(line1, line2)
-    #  print(cross)
-    #  print(line1[0])
-    #  print(line2[0])
+                if not(cross_point(line1, line2)) and slope_compare(line1, line2):
+                    if cross_point(line1, [True, x1, y1, x2, y2]) :
+                        line2 = [True, x1, y1, x2, y2]
+                        cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+                    elif cross_point(line2, [True, x1, y1, x2, y2]):
+                        line1 = [True, x1, y1, x2, y2]
+                        cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 10)
+            #cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 5)
+    if line1[0] and line2[0]:
+        cross = cross_point(line1, line2)
+    else:
+        return None
     if cross:
-        if cross[0] < Screen_Weight and cross[1] < Screem_Height:
+        if (0 < cross[0] < Screen_Weight) and (0 < cross[1] < Screem_Height):
             return True
         else:
             return False
@@ -113,40 +142,50 @@ def line_following(image):
     crop_img = image[int(Screem_Height / 2):Screem_Height, 0:Screen_Weight]
     gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    thresh = cv2.threshold(blur, 60, 255, cv2.THRESH_BINARY_INV)
+    ret, thresh = cv2.threshold(blur, 60, 255, cv2.THRESH_BINARY_INV)
     contours, hierarchy = cv2.findContours(thresh.copy(), 1, cv2.CHAIN_APPROX_NONE)
     if len(contours) > 0:
         c = max(contours, key=cv2.contourArea)
-        m = cv2.moments(c)
-        if m['m00'] == 0:
+        M = cv2.moments(c)
+        if M['m00'] == 0:
             return None
-        cx = int(m['m10'] / m['m00'])
-        cy = int(m['m01'] / m['m00'])
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+        #cv2.line(crop_img, (cx, 0), (cx, Screem_Height), (255, 0, 0), 1)
+        #cv2.line(crop_img, (0, cy), (Screen_Weight, cy), (255, 0, 0), 1)
+        #cv2.drawContours(crop_img, contours, -1, (0, 255, 0), 1)
         deviation = -1 * (cx - Screen_Weight / 2) / (Screen_Weight / 2)
         return deviation
     else:
         return None
 
+def test(image):
+    print(retutn_data)
+    cv2.imshow('Image', image)
+    code = cv2.waitKey(10)
+    if code == 27:
+        return True
+    else:
+        return False
 
-cap = cv2.VideoCapture(0)
+
+cap = cv2.VideoCapture(1)
 Screen_Weight = 720
 Screem_Height = 480
 cap.set(cv2.CAP_PROP_FPS, 30)
 cap.set(3, Screen_Weight)
 cap.set(4, Screem_Height)
-
+retutn_data = []
 while True:
     ret, frame = cap.read()
     if ret:
-        qrdata = decoder(frame)
-        linedata = line_following(frame)
+        #qrdata = decoder(frame)
+        #linedata = line_following(frame)
         detect = cross_detect(frame)
-    #  Testing part
-    '''
-    print("QR: " + str(qrdata))
-    print("Line: " + str(linedata))
-    cv2.imshow('Image', frame)
-    code = cv2.waitKey(10)
-    if code == 27:
-        break
-    '''
+        retutn_data.clear()
+        retutn_data.append(str('Cross : ' + str(detect)))
+        #retutn_data.append(str('QR : ' + str(qrdata)))
+        #retutn_data.append(str('line : ' + str(linedata)))
+
+        if test(frame):
+            break
