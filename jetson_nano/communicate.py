1eb51e4a6307ec1092a916542fd60bf3564a7f2c
Ka1Ra <jack881123@gmail.com>
2021-08-31 22:06:34 +0800

    Update test and modify communicate

--- jetson_nano/communicate.py
+++ jetson_nano/communicate.py
@@ -85,7 +85,7 @@ class Car:
                 print(f'[Receiver] Exception: {e}')
             time.sleep(0.2)
 
-    def set_speed(self, left, right):
+    def run_speed(self, left, right):
         pkg = struct.pack('BBHH', SET_SPEED, 4, left, right)
         cs = 0xff & sum(pkg)
         self.com.write(pkg)
