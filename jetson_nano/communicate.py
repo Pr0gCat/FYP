0924c2c079820b167d4b2cad0cbb77097c949838
ProgCat <progcat@protonmail.com>
2021-10-05 17:35:43 +0800

    python's enum sucks

--- jetson_nano/communicate.py
+++ jetson_nano/communicate.py
@@ -50,7 +50,7 @@ class Car:
             time.sleep(0.2)
 
     def run_speed(self, left, right):
-        pkg = struct.pack('BBhh', self.CommandId.SetMotorSpeed, 4, left, right)
+        pkg = struct.pack('BBhh', int(self.CommandId.SetMotorSpeed), 4, left, right)
         cs = 0xff & sum(pkg)
         self.com.write(pkg)
         self.com.write(struct.pack('B', cs))
