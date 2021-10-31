ca97a0ad36f1095a35131b99f13d66e67599de60
BuildTools <unconfigured@null.spigotmc.org>
2021-10-31 21:32:41 +0800

    del cal.py

--- jetson_nano/Aruco Marker/cal.py
+++
@@ -1,7 +0,0 @@
-import math
-
-print ("5 的平方 = 4 ** 2 = ", 4**2)
-print  ("5 的平方 = math.pow(4,2) = ", math.pow(4,2))
-
-print ("5 的平方根 = 4 ** (0.5) = ", 4**(0.5))
-print  ("5 的平方根 = math.pow(4,0.5) = ", math.pow(4,0.5))
