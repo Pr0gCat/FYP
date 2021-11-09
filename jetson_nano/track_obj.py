86a4cff815bf69cff98a51d21886ddd2e1a81d12
ProgCat <progcat@protonmail.com>
2021-11-09 23:03:02 +0800

    working on tracking object

--- jetson_nano/track_obj.py
+++ jetson_nano/track_obj.py
@@ -6,7 +6,9 @@ import time
 
 def main():
     car = Car()
-    car.init_car()
+    input()
+    car.set_pickup_mode()
+    car.init_car()                                                     
     print('Car initialized')
     with open('camera_cal.npy', 'rb') as f:
         camera_matrix = np.load(f)
@@ -21,7 +23,8 @@ def main():
         arucoFound = findArucoMarkers(img,camera_matrix,camera_distortion)
         if len(arucoFound) > 0 and time.time() - t0 > 100:
             id, cx, cy, dx, dy = arucoFound[0]
-            print('tag found')
+            print(dx, dy)
+            # print('tag found')
             t0 = time.time()
         cv2.imshow("Image", img)
         key = cv2.waitKey(1) & 0xFF
