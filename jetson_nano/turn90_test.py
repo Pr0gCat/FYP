12d025e373c3b61dd19c6d2305181a5fc640d52c
ProgCat <progcat@protonmail.com>
2021-11-17 01:00:47 +0800

    start testing turn 90

---
+++ jetson_nano/turn90_test.py
@@ -0,0 +1,11 @@
+from communicate import Car
+
+if __name__ == '__main__':
+    car = Car()
+    # input()
+    # car.init_car()
+    print("Car initialized")
+    input()
+    car.set_linefollow_mode()
+    print('send')
+    car.run_distance(100, -100)
