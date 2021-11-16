422d892f03187dec6bc0b77b1fc4e62492c339d1
ProgCat <progcat@protonmail.com>
2021-11-17 00:31:34 +0800

    250, 250

--- jetson_nano/line_follow_test.py
+++ jetson_nano/line_follow_test.py
@@ -57,8 +57,8 @@ if __name__ == '__main__':
     while True:
         ret, frame = cap.read()
         MAX_SPPED = 500
-        speed = 300
-        factor = 350
+        speed = 250
+        factor = 250
         if ret:
             if(flag):
                 flag = not findGround(frame, 1)
