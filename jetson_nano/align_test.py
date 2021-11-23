98867dcc383c5ed3b6772f7d8721dae3a05b1f93
ProgCat <progcat@protonmail.com>
2021-11-24 03:17:18 +0800

    a

--- jetson_nano/align_test.py
+++ jetson_nano/align_test.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
             print(offset, speed_l, speed_r)
             if not found:
                 car.run_speed(speed_l, speed_r)
-            if id == 0:
+            elif id == 0:
                 print(flag)
                 car.run_speed(0, 0)
                 time.sleep(2)
@@ -72,9 +72,8 @@ if __name__ == '__main__':
                 time.sleep(5)
                 car.run_distance(-470, 470)
                 time.sleep(5)
-            if id == 1:
-                car.run_speed(0,0)
-                print('done')
+                car.run_distance(1100, 1100)
+                time.sleep(5)
                 break
 
         # time.sleep(0.1)
