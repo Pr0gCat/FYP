a2ccab5cbb81f4cf332a76d3c8dd5f0bbeb57021
ProgCat <progcat@protonmail.com>
2021-11-24 02:59:19 +0800

    test new method

--- jetson_nano/align_test.py
+++ jetson_nano/align_test.py
@@ -1,35 +1,69 @@
 from communicate import Car
-import cv2
-import numpy as np
-import time
 from aruco.find_aruco import findGround
+import cv2
+import time
 
-def main():
-    car = Car()
-    # car.init_car()
-    car.set_linefollow_mode()  
-    car.wait_ack()                                                   
-    print('Car initialized')
+#  Return float or None
+def line_following(img):
+    img = img[int(Screem_Height / 2):Screem_Height, 0:Screen_Weight]
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    ret, thresh = cv2.threshold(blur, 60, 255, cv2.THRESH_BINARY_INV)
+    _, contours, hierarchy = cv2.findContours(
+        thresh, 1, cv2.CHAIN_APPROX_NONE)
+    if len(contours) > 0:
+        c = max(contours, key=cv2.contourArea)
+        M = cv2.moments(c)
+        if M['m00'] == 0:
+            return None
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+        #cv2.line(crop_img, (cx, 0), (cx, Screem_Height), (255, 0, 0), 1)
+        #cv2.line(crop_img, (0, cy), (Screen_Weight, cy), (255, 0, 0), 1)
+        #cv2.drawContours(crop_img, contours, -1, (0, 255, 0), 1)
+        deviation = -1 * (cx - Screen_Weight / 2) / (Screen_Weight / 2)
+        return deviation
+    else:
+        return None
 
-    cap = cv2.VideoCapture(-1)
-    TOL = 20
-    while True:
-        success, img = cap.read()
-        if not success:
-            continue
-        found, id, rot = findGround(img)
-        if not found:
-            car.run_speed(100,-100)
-            continue
-        print(rot)
-        if rot < -10 :
-            car.run_speed(100,-100)
-        elif rot > 10:
-            car.run_speed(-100,100)
-        else:
-            car.run_speed(0,0)
-
-    cap.release()
 
 if __name__ == '__main__':
-    main()
+    car = Car()
+    # input()
+    # car.init_car()
+    # car.wait_ack()
+    # print("Car initialized")
+    car.set_linefollow_mode()
+    car.wait_ack()
+    input('ready')
+    cap = cv2.VideoCapture(0)
+    Screen_Weight = 720
+    Screem_Height = 480
+    cap.set(cv2.CAP_PROP_FPS, 30)
+    cap.set(3, Screen_Weight)
+    cap.set(4, Screem_Height)
+    retutn_data = []
+    flag = True
+    while True:
+        ret, frame = cap.read()
+        MAX_SPPED = 400
+        speed = 200
+        factor = 300
+        if ret:
+            found, id, rot = findGround(frame)
+            offset = line_following(frame)
+            # print(offset)
+            if offset is None:
+                # print('no line')
+                continue
+            compan = int(offset * factor)
+            speed_l = min(MAX_SPPED, speed - compan)
+            speed_r = min(MAX_SPPED, speed + compan)
+            print(offset, speed_l, speed_r)
+            if not found:
+                car.run_speed(speed_l, speed_r)
+            elif id == 0:
+                print(flag)
+                car.run_speed(0, 0)
+
+        # time.sleep(0.1)
