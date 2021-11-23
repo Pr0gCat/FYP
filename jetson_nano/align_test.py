14f9d0abb29ed3415d31941aae7a3f5229f26421
ProgCat <progcat@protonmail.com>
2021-11-24 04:09:04 +0800

    auto commit, testing

--- jetson_nano/align_test.py
+++ jetson_nano/align_test.py
@@ -31,8 +31,8 @@ def line_following(img):
 if __name__ == '__main__':
     car = Car()
     # input()
-    # car.init_car()
-    # car.wait_ack()
+    car.init_car()
+    car.wait_ack()
     print("Car initialized")
     car.set_linefollow_mode()
     car.wait_ack()
