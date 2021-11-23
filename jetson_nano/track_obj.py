ae51243c5a1956b18a1dbfc9f550f46a7b0926a9
ProgCat <progcat@protonmail.com>
2021-11-24 06:10:49 +0800

    auto commit, testing

--- jetson_nano/track_obj.py
+++ jetson_nano/track_obj.py
@@ -11,25 +11,21 @@ def main():
     car.set_pickup_mode()                                                     
     print('Car initialized')
 
-    with open('camera_cal.npy', 'rb') as f:
-        camera_matrix = np.load(f)
-        camera_distortion = np.load(f)
-
     cap = cv2.VideoCapture(-1)
     t0 = time.time()
     while True:
         success, img = cap.read()
         if not success:
             continue
-        arucoFound = findArucoMarkers(img,camera_matrix,camera_distortion)
+        arucoFound = findArucoMarkers(img)
         if len(arucoFound) > 0 and time.time() - t0 > 1:
-            id, cx, cy, dx, dy = arucoFound[0]
-            print(dx, dy)
-            # print('tag found')
-            if dy < -30:
+            id, cx, cy, _ = arucoFound[0]
+            dist = cy - 240
+            print('tag found')
+            if dist < -30:
                 print('go up')
                 car.move_posz(5)
-            elif dy > 30:
+            elif dist > 30:
                 print('go down')
                 car.move_posz(-5)
             t0 = time.time()
