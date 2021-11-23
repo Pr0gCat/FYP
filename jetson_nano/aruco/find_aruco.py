d9a47c2ba2b75ef586d90d583d2ee20c1c9214ac
Paco Deng <pacodeng@Pacos-MacBook-Pro.local>
2021-11-23 23:03:04 +0800

    check

--- jetson_nano/aruco/find_aruco.py
+++ jetson_nano/aruco/find_aruco.py
@@ -36,7 +36,7 @@ def rotationMatrixToEulerAngles(R):
     
     return np.array([x, y, z])
 
-def findArucoMarkers(img, cargo, markerSize=6, totalMarkers=250):
+def findArucoMarkers(img, cargo):
     data=[]
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     corners, ids, rejected = aruco.detectMarkers(imgGray, arucoDict, parameters=arucoParam)
@@ -46,7 +46,7 @@ def findArucoMarkers(img, cargo, markerSize=6, totalMarkers=250):
                 return True
     return False
 
-def findGround(img, markerSize=6, totalMarkers=1):
+def findGround(img):
     img = img[int(Screem_Height / 3):Screem_Height, 0:Screen_Weight]
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     corners, ids, rejected = aruco.detectMarkers(imgGray, arucoDict, parameters=arucoParam)
