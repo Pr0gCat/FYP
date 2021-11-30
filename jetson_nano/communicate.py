8f3ab72e3300fe436669a2cce247b31a51de8e44
ProgCat <progcat@protonmail.com>
2021-11-30 22:48:36 +0800

    add debug message

--- jetson_nano/communicate.py
+++ jetson_nano/communicate.py
@@ -67,6 +67,7 @@ class Car:
                         buff.append(data)
                         t0 = time.time()
                         data_len += 1
+                        print(data_len, buff[1])
                         if data_len == buff[1]:
                             flag = 3
                     elif flag == 3:
