682b52b5a9976bbecf8f4c23221decdecf1ff08d
ProgCat <progcat@protonmail.com>
2021-12-01 01:41:22 +0800

    init

--- jetson_nano/debug_commun.py
+++ jetson_nano/debug_commun.py
@@ -2,7 +2,7 @@ from communicate import Car
 
 if __name__ == '__main__':
     car = Car()
-    # car.init_car()
+    car.init_car()
     print('Car initialized')
     car.move_posz(300)
     car.move_posz(100)
