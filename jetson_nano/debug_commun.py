e6102dc57628a30426b224ee9aff7203b2f66016
ProgCat <progcat@protonmail.com>
2021-12-01 00:08:09 +0800

    test faster

--- jetson_nano/debug_commun.py
+++ jetson_nano/debug_commun.py
@@ -2,6 +2,9 @@ from communicate import Car
 
 if __name__ == '__main__':
     car = Car()
-    car.init_car()
-    car.wait_ack(120)
+    car.set_pickup_mode()
+    car.wait_ack(2)
     print('Car initialized')
+    input()
+    car.set_linefollow_mode()
+    car.wait_ack(2)
